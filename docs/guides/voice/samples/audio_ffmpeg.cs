f3ff8506ee0b0da12b5071bfebbe3ef068eab867
Luke Tomkus <luke@pkillboredom.com>
2023-03-26 14:02:09 -0400

    Update samples/audio_ffmpeg.cs
    Adds a cancellation token and event handler for IAudioClient Disconnected.

--- docs/guides/voice/samples/audio_ffmpeg.cs
+++ docs/guides/voice/samples/audio_ffmpeg.cs
@@ -1,11 +1,18 @@
 private async Task SendAsync(IAudioClient client, string path)
 {
+    // Create a cancellation token and handle forced disconnects by users.
+    CancellationTokenSource ctxSource = new CancellationTokenSource();
+    client.Disconnected += (ex =>
+    {
+        ctxSource.Cancel();
+        return Task.CompletedTask;
+    });
     // Create FFmpeg using the previous example
     using (var ffmpeg = CreateStream(path))
     using (var output = ffmpeg.StandardOutput.BaseStream)
     using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
     {
-        try { await output.CopyToAsync(discord); }
-        finally { await discord.FlushAsync(); }
+        try { await output.CopyToAsync(discord, ctxSource.Token); }
+        finally { await discord.FlushAsync(ctxSource.Token); }
     }
 }
